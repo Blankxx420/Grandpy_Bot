82b706cb038266afb8c6ab584164313a8186e967
blankxx420 <briceguichou.dev@gmail.com>
2021-04-27 14:03:44 +0200

    initializing flask

--- static/Javascript/index.js
+++ static/Javascript/index.js
@@ -2,7 +2,7 @@ $('#ask-question').on('keypress click', function(){
     console.log("Tu as cliqué sur le bouton !")
     let questionText = $('#question-value').val()
     console.log("La question posée est :", questionText)
-    let question = "<div class='chatbox col-12 asker text-right'>" + questionText + '<img src="../Images/user_avatar.png" class="float-right" alt="user_avatar" width="50">' + "</div>"
+    let question = "<div class='chatbox col-12 asker text-right'>" + questionText + '<img src="../static/Images/user_avatar.png" class="float-right" alt="user_avatar" width="50">' + "</div>"
     $('#all-chatbox').append(question)
 })
 
