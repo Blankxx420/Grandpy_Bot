5c406bacec2b05691dcd39df1a6198c4416ed8d6
blankxx420 <briceguichou.dev@gmail.com>
2021-04-29 16:24:41 +0200

    creating parser.py

---
+++ Bot_app/parser.py
@@ -0,0 +1,6 @@
+import json
+
+
+class Parser:
+    def __init__(self):
+        self.stop_word = 'stopword.json'
--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
 GP_APP_ID = 1200420960103822
+
