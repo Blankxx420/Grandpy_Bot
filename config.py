66e19445a9aabd20afaf44e2247ab064cd8e3afe
blankxx420 <briceguichou.dev@gmail.com>
2021-05-27 15:06:54 +0200

    adding variable environments

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
-GP_APP_ID = 1200420960103822
+GEO_API_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+GEO_TOKEN = os.environ.get("GEOLOC_TOKEN")
 
 PUNCTUATION = ["'", '"', ",", ".", "!", ":", ";", "?"]
