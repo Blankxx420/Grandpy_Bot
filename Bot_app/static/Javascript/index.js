3440be12606d6ca0c0517d63a0d4f6b80fd09ce4
blankxx420 <briceguichou.dev@gmail.com>
2021-06-17 14:28:49 +0200

    working on request data in js

--- Bot_app/static/Javascript/index.js
+++ Bot_app/static/Javascript/index.js
@@ -1,7 +1,5 @@
 $('#ask-question').on('click', function(){
-    console.log("Tu as cliqué sur le bouton !")
     const questionText = $('#question-value').val()
-    console.log("La question posée est :", questionText)
     let questions = "<div class='chatbox col-12 asker text-right'>" + questionText + '<img src="../static/Images/user_avatar.png" class="float-right" alt="user_avatar" width="50">' + "</div>"
     $('#all-chatbox').append(questions)
     $.ajax({
@@ -10,29 +8,15 @@ $('#ask-question').on('click', function(){
     url: '/question',
     dataType: 'text',
     success: function (response){
-            console.log(response)
             let details = JSON.parse(response);
             console.log(details)
             if("sentence" in details){
                 const longitude = details.longitude;
-                console.log(longitude)
                 const latitude = details.latitude;
-                console.log(latitude)
                 const wiki_url = details.url;
                 const sentence = details.sentence;
-                let div_map = "<div id='map' style='width: 400px; height: 300px;'>"
-                let bot_response = "<div class='chatbox col-12 answerer text-left'>" + "<img src='../static/Images/logo_robot.png' alt='avatar' width='50'>" + sentence + "<a href=" + wiki_url+"> Plus d'infos<a/>" + div_map + "</div"
-                $('#all-chatbox').append(bot_response)
-                mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhbmt4eCIsImEiOiJja3A1bm40bjYwaWs5MnRxdHdua2JiazNyIn0.iFNN7eAz04uWKAazMhjHQQ';
-                var map = new mapboxgl.Map({
-                        container: "map",
-                        style: 'mapbox://styles/mapbox/streets-v11',
-                        center: [longitude, latitude],
-                        zoom: 5,
-                });
-                var marker = new mapboxgl.Marker()
-                .setLngLat([longitude, latitude])
-                .addTo(map)
+
+
             }
     },
     error:  function(error){
