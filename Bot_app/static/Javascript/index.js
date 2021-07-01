f4fc34843cb5c5746451358aef167109029a5d2a
Blankxx420 <briceguichou.dev@gmail.com>
2021-07-02 00:53:26 +0200

    preparing deployment

--- Bot_app/static/Javascript/index.js
+++ Bot_app/static/Javascript/index.js
@@ -1,16 +1,17 @@
 import {Map} from "./map.js";
 
-$('#ask-question').on('click', function(){
+$('#ask-question').on('click', function(){  // get event from user
 
-    const questionText = $('#question-value').val()
+    const questionText = $('#question-value').val() //get the value of the user input
 
-    let questions = "<div class='chatbox col-12 asker text-right'>"
+    let questions = "<div class='chatbox col-12 asker text-right'>" //creating div for chatbox
         + questionText + '<img src="../static/Images/user_avatar.png"' +
         ' class="float-right" alt="user_avatar" width="50">'
         + "</div>"
 
-    $('#all-chatbox').append(questions)
+    $('#all-chatbox').append(questions) //adding the div in index.html
 
+    // Creating ajax call for backend and send question of user
     $.ajax({
     data: {'question': questionText},
     type: 'POST',
@@ -22,20 +23,21 @@ $('#ask-question').on('click', function(){
             console.log(details)
 
             if("sentence" in details){
+                //get the data from response
                 const longitude = details.longitude;
                 const latitude = details.latitude;
                 const wiki_url = details.url;
                 const sentence = details.sentence;
 
-                let bot_r_url = "<a href=" + wiki_url + ">" + "plus d'infos"  + "</a>"
+                let bot_r_url = "<a href=" + wiki_url + ">" + "plus d'infos"  + "</a>" //creating link div
 
                 let bot_response = "<div class='chatbox col-12 answerer text-left'>"+
-                    '<img src="../static/Images/logo_robot.png" alt="avatar" width="50">'
+                    '<img src="../static/Images/logo_robot.png" alt="avatar" width="50">' // create bot response
                     + sentence+ "\n" + bot_r_url + "</div>"
 
-                new Map(longitude, latitude)
+                new Map(longitude, latitude) //create the map
 
-                $("#all-chatbox").append(bot_response)
+                $("#all-chatbox").append(bot_response) // adding the Bot response in chat
 
             }
     },
